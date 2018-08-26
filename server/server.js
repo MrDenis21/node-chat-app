112143ff31227fe4ee6175afc36516fa302ea787
Denis <MrDenis21@mail.ru>
2018-08-26 13:49:49 +0300

    socket.io added on client and server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = 3000 || process.env.PORT ; 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log("server is started on port 3000");
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User is disconnected');
+    })
 })
+
+server.listen(3000, () => {
+    console.log("server is started on port 3000");
+});
